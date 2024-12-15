41cde7b0559c45adc52056ca171607fe801092f0
jyto7034 <jyto7034@naver.com>
2024-12-15 22:43:39 +0900

    refactor: add null handling

--- src/lib/components/profile.svelte
+++ src/lib/components/profile.svelte
@@ -5,6 +5,9 @@
 	let selectedArtists: boolean[] = [];
 
 	$: cache = data;
+	$: if (cache) {
+		console.log(cache);
+	}
 
 	$: if (
 		$current_filter_type != $previous_filter_type ||
@@ -25,38 +28,40 @@
 
 <div class="flex flex-wrap justify-center gap-4">
 	{#each cache || [] as item, i}
-		<button
-			on:click={() => toggleSelection(i)}
-			class="btn relative h-32 w-32 border-none bg-transparent p-0 transition-all duration-200 hover:-translate-y-1"
-		>
-			<div class="relative h-full w-full {selectedArtists[i]}">
-				<!-- 어두운 오버레이 -->
-				{#if !selectedArtists[i]}
-					<div
-						class="absolute inset-0 z-10 rounded-xl {$global_theme === 'dark'
-							? 'bg-black/60'
-							: 'bg-black/30'} transition-opacity duration-200"
-					/>
-				{/if}
+		{#if item && item.id && item.name}
+			<button
+				on:click={() => toggleSelection(i)}
+				class="btn relative h-32 w-32 border-none bg-transparent p-0 transition-all duration-200 hover:-translate-y-1"
+			>
+				<div class="relative h-full w-full {selectedArtists[i]}">
+					<!-- 어두운 오버레이 -->
+					{#if !selectedArtists[i]}
+						<div
+							class="absolute inset-0 z-10 rounded-xl {$global_theme === 'dark'
+								? 'bg-black/60'
+								: 'bg-black/30'} transition-opacity duration-200"
+						/>
+					{/if}
 
-				<img
-					src={`/images/idol/${item.id}.webp`}
-					class="h-full w-full rounded-lg object-cover {selectedArtists[i] &&
-					$global_theme === 'dark'
-						? 'brightness-125'
-						: ''}"
-					alt="thumbnail"
-				/>
-				<!-- 텍스트를 위한 그라데이션 배경 -->
-				<div
-					class="absolute bottom-0 left-0 right-0 h-12 rounded-b-lg bg-gradient-to-t from-black/70 to-transparent"
-				/>
-				<span
-					class="absolute bottom-2 left-0 right-0 z-20 text-center font-medium text-white drop-shadow-lg"
-				>
-					{item.name}
-				</span>
-			</div>
-		</button>
+					<img
+						src={`/images/idol/${item.id}.webp`}
+						class="h-full w-full rounded-lg object-cover {selectedArtists[i] &&
+						$global_theme === 'dark'
+							? 'brightness-125'
+							: ''}"
+						alt="thumbnail"
+					/>
+					<!-- 텍스트를 위한 그라데이션 배경 -->
+					<div
+						class="absolute bottom-0 left-0 right-0 h-12 rounded-b-lg bg-gradient-to-t from-black/70 to-transparent"
+					/>
+					<span
+						class="absolute bottom-2 left-0 right-0 z-20 text-center font-medium text-white drop-shadow-lg"
+					>
+						{item.name}
+					</span>
+				</div>
+			</button>
+		{/if}
 	{/each}
 </div>
